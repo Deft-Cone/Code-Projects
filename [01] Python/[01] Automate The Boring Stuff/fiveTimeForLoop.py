2705e6bd949ae6c3959a020c53679cb4a98d3194
Lane Alexander <51537771+Deft-Cone@users.noreply.github.com>
2024-08-28 01:46:34 -0400

    First Commit

---
+++ [01] Python/[01] Automate The Boring Stuff/fiveTimeForLoop.py
@@ -0,0 +1,16 @@
+##print('My name is')
+##for i in range (5):
+##    print('Jimmy Five Time ' + str(i))
+
+##total = 0
+##for num in range(101):
+##    total = total + num
+##print(total)
+
+##print('My name is')
+##for i in range (5, 11):
+##    print('Jimmy Five Time ' + str(i))
+
+print('My name is')
+for i in range (10, -1, -1):
+    print('Jimmy Five Time ' + str(i))
